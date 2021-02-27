30aa0eccde21504e122e22b5f906973ef28c24d6
Ana Paula Gomes <apgomes88@gmail.com>
2021-02-27 19:52:49 +0100

    Adiciona pacote de dados do Diário Oficial

---
+++ datasets/diarios-oficiais/query.sql
@@ -0,0 +1,13 @@
+SELECT
+	dg.id, dg.crawled_at, dg.crawled_from, dg.notes, dg.date, dg.power, dg.year_and_edition, dg.is_legacy,
+	json_agg(
+            json_build_object(
+                'title', event.title,
+                'secretariat', event.secretariat,
+                'summary', event.summary,
+                'published_on', event.published_on
+			)
+        ) as events
+FROM public.datasets_gazette dg
+LEFT JOIN public.datasets_gazetteevent event ON (dg.id = event.gazette_id)
+GROUP BY dg.id
